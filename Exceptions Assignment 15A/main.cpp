53fa9332f8df2641a74a9000f359b8bc08d62e37
Alex Rudy <alex.rudy@sinclair.edu>
2023-04-30 17:12:30 -0400

    Exception classes and Function definition

--- Exceptions Assignment 15A/main.cpp
+++ Exceptions Assignment 15A/main.cpp
@@ -6,4 +6,19 @@
 #include <string>
 using namespace std;
 
+class invalidCharacterException{};
+class invalidRangeException{};
+
+char character(char start, int offset)
+{
+    if((start < 65 || start > 90) && (start < 97 || start > 122))
+        throw invalidCharacterException();
+    
+    int n = start + offset;
+    
+    if((n < 65 || n > 90) && (n < 97 || n > 122))
+        throw invalidRangeException();
+    
+    return char(n);
+}
 
