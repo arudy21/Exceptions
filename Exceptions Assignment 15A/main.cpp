daa9765078f52f02b759ae1c646c3212c60e9e49
Alex Rudy <alex.rudy@sinclair.edu>
2023-04-30 16:43:48 -0400

    Initial Commit

---
+++ Exceptions Assignment 15A/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Exceptions Assignment 15A
+//
+//  Created by Alex Rudy on 4/30/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
